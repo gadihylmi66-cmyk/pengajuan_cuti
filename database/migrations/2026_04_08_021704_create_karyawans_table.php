c546c1df232149a4016c46a97e0fc1241f7b67fb
gadi <gadihylmi66@gmail.com>
2026-04-08 10:26:48 +0700

    first

---
+++ database/migrations/2026_04_08_021704_create_karyawans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_karyawan');
+            $table->foreignId('id_jabatan')->constrained('jabatans')->onDelete('cascade');
+            $table->string('jenis_kelamin');
+            $table->string('umur');
+            $table->string('ttl');
+            $table->string('agama');
+            $table->string('alamat');
+            $table->string('no_telp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawans');
+    }
+};
